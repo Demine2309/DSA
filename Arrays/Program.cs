7900017e9e0cb07fbf25882c9326c693d411bd8c
Demine Nguyen <cuonghustmi1@gmail.com>
2023-09-30 19:36:14 +0700

    Demine
    30/09/2023

---
+++ Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DSA
+{
+    #region Jagged Array
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArr = new int[][]
+            {
+                new int[] { 1,2,3,4},
+                new int[] { 5, 6,9},
+                new int[] { 89, 23},
+                new int[] {0,45,78,53,99}
+            };
+
+            for (int n = 0; n < jaggedArr.Length; n++)
+            {
+                Console.Write($"Row({n}): ");
+
+                for(int k =0;  k < jaggedArr[n].Length; k++)
+                {
+                    Console.Write($"{jaggedArr[n][k]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    #endregion
+}
