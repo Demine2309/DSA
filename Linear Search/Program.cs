031bd0ee69dee7b445b81c096a6407b59d7d0d2c
Cuong <cuonghustmi1@gmail.com>
2023-12-20 01:11:54 +0700

    Demine
    20/12/2023

---
+++ Linear Search/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DSA
+{
+    public class Solution
+    {
+        public int LinearSearch(int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] arr = { 10, 50, 30, 70, 80, 20, 90, 40 };
+            int key = 30;
+
+            int result = solution.LinearSearch(arr, key);
+
+            if (result == -1)
+                Console.WriteLine("No match found");
+            else
+                Console.WriteLine($"The location of the key is located in the location: {result + 1}");
+        }
+    }
+}
