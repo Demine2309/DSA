ad862cb23a251d3b0b08728217d5ed960e30d0c6
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-02-22 21:28:39 +0700

    Demine
    22/02/2024

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -2,7 +2,22 @@
 {
     public class Solution
     {
+        public int BinarySearch(int[] arr, int low, int high, int key)
+        {
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+               
+                if (arr[mid] < key)
+                    low = mid + 1;
+                else if (arr[mid] > key)
+                    high = mid - 1;
+                else
+                    return mid;
+            }
 
+            return -1;
+        }
     }
 
     class MainClass
@@ -11,7 +26,20 @@
         {
             Solution solution = new Solution();
 
+            int[] arr = { 7, 31, 5, 2, 3, 54, 23, 6, 1, 0, 4, 8 };
+           
+            Console.WriteLine("Array before sort:");
+            foreach (int i in arr) Console.Write(i + " ");
+            Console.WriteLine();
 
+            Console.WriteLine("Array after sort:");
+            Array.Sort(arr);
+            foreach (int i in arr) Console.Write(i + " ");
+            Console.WriteLine();
+            
+            int target = 8;
+            Console.WriteLine("The position of target...");
+            Console.WriteLine(solution.BinarySearch(arr, 0, arr.Length-1,target) + 1);
         }
     }
 }
