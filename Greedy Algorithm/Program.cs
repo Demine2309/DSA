79afc5ec2f5f446f97411f2d31467ad0bd425c84
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-02-25 21:37:29 +0700

    Demine
    25/02/2024

--- Greedy Algorithm/Program.cs
+++ Greedy Algorithm/Program.cs
@@ -3,21 +3,46 @@
     public class Solution
     {
         #region Example: Being a very busy person, you have exactly T time to do some interseting things and you want to do maximum such things
-        public int NumberOfThing(int[] A, int T)
-        {
-            int currentTime = A[0];
-            int numberOfThing = 0;
+        //public int NumberOfThing(int[] A, int T)
+        //{
+        //    int currentTime = A[0];
+        //    int numberOfThing = 0;
 
-            for(int i = 1; i < A.Length; i++)
+        //    for(int i = 1; i < A.Length; i++)
+        //    {
+        //        if(currentTime <= T)
+        //        {
+        //            currentTime += A[i];
+        //            numberOfThing++;
+        //        }
+        //    }
+
+        //    return numberOfThing;
+        //}
+        #endregion
+
+        #region The scheduling problem
+        public double CalculateObjectFunction(int[] P, int[] T, int N)
+        {
+            double[] S = new double[N]; // Array to store the scores
+            double C = 0;               // Completion times
+            double F = 0;               // Objective function
+
+            for (int i = 0; i < N; i++)
             {
-                if(currentTime <= T)
-                {
-                    currentTime += A[i];
-                    numberOfThing++;
-                }
+                S[i] = (double)P[i] / T[i];
             }
 
-            return numberOfThing;
+            Array.Sort(S);
+
+            for (int i = 0; i < N; ++i)
+            {
+                int index = Array.IndexOf(S, (double)P[i] / T[i]);
+                C += T[index];
+                F += P[index] * C;
+            }
+
+            return F;
         }
         #endregion
     }
@@ -26,14 +51,28 @@
     {
         static void Main(string[] args)
         {
+            #region Example 1
+            //Solution solution = new Solution();
+
+            //int[] A = { 5, 3, 4, 2, 1 };
+            //Array.Sort(A);
+
+            //int T = 6;
+
+            //Console.WriteLine(solution.NumberOfThing(A, T));
+            #endregion
+
+            #region Example 2
             Solution solution = new Solution();
 
-            int[] A = { 5, 3, 4, 2, 1 };
-            Array.Sort(A);
+            int[] P = { 3, 1 };
+            int[] T = { 5, 2 };
+            int N = P.Length;
 
-            int T = 6;
+            double result = solution.CalculateObjectFunction(P, T, N);
 
-            Console.WriteLine(solution.NumberOfThing(A, T));
+            Console.WriteLine(result);
+            #endregion
         }
     }
 }
