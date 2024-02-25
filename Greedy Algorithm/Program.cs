b5f5f1fd1608fb4d4a5b500d69f3008dca9930a6
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-02-25 17:10:24 +0700

    Demine
    25/02/2024

---
+++ Greedy Algorithm/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DSA
+{
+    public class Solution
+    {
+        #region Example: Being a very busy person, you have exactly T time to do some interseting things and you want to do maximum such things
+        public int NumberOfThing(int[] A, int T)
+        {
+            int currentTime = A[0];
+            int numberOfThing = 0;
+
+            for(int i = 1; i < A.Length; i++)
+            {
+                if(currentTime <= T)
+                {
+                    currentTime += A[i];
+                    numberOfThing++;
+                }
+            }
+
+            return numberOfThing;
+        }
+        #endregion
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] A = { 5, 3, 4, 2, 1 };
+            Array.Sort(A);
+
+            int T = 6;
+
+            Console.WriteLine(solution.NumberOfThing(A, T));
+        }
+    }
+}
