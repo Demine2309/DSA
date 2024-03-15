194e1f19d9992099c0c8e15117b418aa5c42975b
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-03-15 23:46:08 +0700

    Demine
    15/03/2024

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -46,9 +46,123 @@
     #endregion
 
     #region Binary search tree
-    public class Solution
+    public class Node
     {
-        public 
+        public int _data;
+        public Node left;
+        public Node right;
+
+        public Node(int data)
+        {
+            _data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            root = InsertRecursive(root, data);
+        }
+
+        private Node InsertRecursive(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if (data < root._data)
+            {
+                root.left = InsertRecursive(root.left, data);
+            }
+            else if (data > root._data)
+            {
+                root.right = InsertRecursive(root.right, data);
+            }
+
+            return root;
+        }
+
+        public Node FindNodeIterative(int item)
+        {
+            Node node = root;
+
+            // Loop untill we descend past the closest leaf node
+            while (node != null)
+            {
+                if (item == node._data)
+                {
+                    return node;
+                }
+                else if (item < node._data)
+                {
+                    node = node.left;
+                }
+                else if (item > node._data)
+                {
+                    node = node.right;
+                }
+            }
+
+            // Not found
+            return null;
+        }
+
+        public void InorderTraversal()
+        {
+            InorderRecursive(root);
+        }
+
+        private void InorderRecursive(Node root)
+        {
+            if(root != null)
+            {
+                InorderRecursive(root.left);
+                Console.WriteLine(root._data + " ");
+                InorderRecursive(root.right);
+            }
+        }
+    }
+
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(60);
+            bst.Insert(30);
+            bst.Insert(20);
+            bst.Insert(40);
+            bst.Insert(70);
+            bst.Insert(60);
+            bst.Insert(80);
+
+            Console.WriteLine("Inorder Traversal of the BST:");
+            bst.InorderTraversal();
+            Console.WriteLine();
+
+            int searchKey = 54;
+            Node foundNode = bst.FindNodeIterative(searchKey);
+            if (foundNode != null)
+            {
+                Console.WriteLine($"Node with data {searchKey} found in the BST.");
+            }
+            else
+            {
+                Console.WriteLine($"Node with data {searchKey} not found in the BST.");
+            }
+        }
     }
     #endregion
 }
