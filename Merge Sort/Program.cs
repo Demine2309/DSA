cfecc79d65e254fbad7fcac5e0af6c5612248831
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-03-18 23:34:59 +0700

    Demine
    18/03/2024

--- Merge Sort/Program.cs
+++ Merge Sort/Program.cs
@@ -159,6 +159,8 @@
  * 1. Quick Sort is not a stable sorting algorithm in its typical implementation
  * 2. A: You have to sort 1 GB of data with only 100 MB of available main memory. Which sorting technique will be most appropriate? 
  *    Q: Merge sort
- *    Explain: 
+ *    Explain: The data can be sorted using external sorting which uses merging technique. This can be done as follow: 1. Divide the data into 10 groups each of size 100
+ *             2. Sort each group and write them to disk, 3. Load 10 items from each group into main memory, 4. Output the smallest item from the main memory to disk. Load the
+ *             next item from the group whose item was chosen, 5. Loop step #4 until all items are not outputted. The step 3-5 is called as merging technique.
  */
 #endregion
