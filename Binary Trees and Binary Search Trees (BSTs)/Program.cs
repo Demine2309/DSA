3e844b7f2231f1ce2b0374d73a7ada7b0928df66
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-03-25 23:34:55 +0700

    Demine
    25/04/2024

--- Binary Trees and Binary Search Trees (BSTs)/Program.cs
+++ Binary Trees and Binary Search Trees (BSTs)/Program.cs
@@ -2,7 +2,33 @@
 {
 	#region Learn Data structure & Algorithm with William Fiset
 	/*
-	 * 
+	 * 1. Binary search Tree introduction
+	 * * Quick terminology crash course
+	 * - A tree is an undirected graph which satisfies any of the following definitions:
+	 *		+ An acyclic connnected graph
+	 *		+ A connected graph with N nodes and N-1 edges
+	 *		+ An graph in which any two vertices are connected by exactly one path
+	 * - If we have a rooted tree then we will want to have a reference to the root node of our tree.
+	 * - It does not always matter which node is selected to be the root node because any does can root the tree!
+	 * - A child is a node extending from another node. A parent is the inverse of this
+	 * - A leaf node í a node with no children.
+	 * * Which is a Binary Tree (BT)
+	 * - A binary tree us a tree for which every node has at most two child nodes.
+	 * * Which is a Binary Seach Tree (BST)?
+	 * - A binary search tree is a binary tree that satisfies the BST invariant: left subtree has smaller elements and
+	 *	 right subtree has large elements.
+	 * - BST operations allow for duplicate values, but most of the time we are only interested in having unique elements 
+	 *	 inside our tree.
+	 * * When and Where are Binary Trees used?
+	 * - Binary Search Trees (BSTs)
+	 *		+ Implementation of some map and sets ADTs
+	 *		+ Red Black Trees
+	 *		+ AVL Trees
+	 *		+ Splay Trees
+	 *		+ etc...
+	 * - Used in the implementation of binary heaps
+	 * - Syntax trees (used by compiler and calculators)
+	 * - Treap - a probabilistic DS (uses a randomized BST)
 	 */
 	#endregion
 }
