524bcb6c4d544b78c5d095f4153c4e99ba5f5bc0
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-03-28 21:47:45 +0700

    Demine
    28/03/2024

--- Binary Trees and Binary Search Trees (BSTs)/Program.cs
+++ Binary Trees and Binary Search Trees (BSTs)/Program.cs
@@ -29,6 +29,17 @@
 	 * - Used in the implementation of binary heaps
 	 * - Syntax trees (used by compiler and calculators)
 	 * - Treap - a probabilistic DS (uses a randomized BST)
+	 * 2. Inserting elements into a Binary Search Tree (BST)
+	 * * Adding elements to a BST
+	 * - Binary Search Tree (BST) elements must be comparable so that we can order then inside the tree
+	 * - When inserting an element we want to compare its value to the value stored in the current node we're considering 
+	 *   to decide on one of the following:
+	 *		+ Recurse down left subtree        (< case)
+	 *		+ Recurse down right subtree       (> case)
+	 *		+ Handle finding a duplicate value (= case)
+	 *		+ Create a new node                (found a null leaf)
+	 * - On average the insertion time will be logarithmic, but in the worst case this could degrade to linear time.
+	 * 3. 
 	 */
 	#endregion
 }
