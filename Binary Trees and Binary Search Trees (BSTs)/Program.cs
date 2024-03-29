6a21d97d981e8283cd76462822ea33ad1d9d2927
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-03-29 11:22:13 +0700

    Demine
    29/03/2024

--- Binary Trees and Binary Search Trees (BSTs)/Program.cs
+++ Binary Trees and Binary Search Trees (BSTs)/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace DSA
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DSA
 {
-    #region Learn Data structure & Algorithm with William Fiset
+    #region Learn Data structure & Algorithm with William Fiset (Theory)
     /*
 	 * 1. Binary search Tree introduction
 	 * * Quick terminology crash course
@@ -68,7 +71,237 @@
 	 *			1. Is smaller than everything is right subtree. This follows immediately from the definition of being the smallest
 	 *			2. Is large than everything in left subtree because it was found in the right subtree.
 	 * 4. Tree traversals (Preorder, Inorder, Postorder & Level order)
-	 * 
+	 * * Preorder traversal
+	 * - Preorder prints before the recursive calls
+	 * - Print the value of the current node them traverse the left subtree followed by the right subtree
+	 * * Inorder traversal
+	 * - Inorder prints between the recursive calls
+	 * - Traverse the left subtree, then print the value of the node and continue traversing the right subtree
+	 * - Notice that with a BST the value printed by the inorder traversal are in increasing order!
+	 * + Postorder Traversal
+	 * - Postorder prints after the recursive calls
+	 * - Traverse the left subtree followed by the right subtree then print the value of the node
+	 * * Level order traversal
+	 * - In a level order traversal we want to print the nodes as they appear one layer at a time
+	 * - To obtain this ordering we want to do a Breadth First Search (BFS) from the root node down to the leaf nodes
+	 * - To do a BFS we will need to maintain a Queue of the nodes left to explore
+	 * - Begin with the root inside of the queue and finish when the queue is empty
+	 * - At each iteration we add the left child and then the right child of the current node to out Queue.
 	 */
+    #endregion
+
+    #region Learn Data structure & Algorithm with William Fiset (Code)
+    public class BinarySearchTree
+    {
+        // Tracks the number of nodes in this BST
+        private int nodeCount = 0;
+
+        // This BST is a rooted tree so we maintain a handle on the root node
+        private TreeNode root = null;
+
+
+        // Internal node containing node references
+        // and the actual node data
+        private class TreeNode
+        {
+            public int data;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(TreeNode left, TreeNode right, int value)
+            {
+                this.data = value;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        // Check if this binary tree is empty
+        public bool IsEmpty()
+        {
+            return Size() == 0;
+        }
+
+        // Get the number of nodes in this binay tree
+        public int Size()
+        {
+            return nodeCount;
+        }
+
+        // Add an element to this binary tree. Returns true if we successfully perform an insertion
+        public bool Add(int element)
+        {
+            // Check if	the value already exists in this binary tree, if it does ignore adding it
+            if (Contains(element))
+                return false;
+
+            // Otherwise add this element to the binary tree  
+            else
+            {
+                root = Add(root, element);
+                nodeCount++;
+                return true;
+            }
+        }
+
+        // Private method to recursively add a value in the binary tree 
+        private TreeNode Add(TreeNode node, int element)
+        {
+            // Base case: found a leaf node
+            if (node == null)
+                node = new TreeNode(null, null, element);
+
+            // Place lower elements values in left subtree 
+            else
+            {
+                if (element.CompareTo(node.data) < 0)
+                    node.left = Add(node.left, element);
+                else
+                    node.right = Add(node.right, element);
+            }
+
+            return node;
+        }
+
+        // Remove a value from this binary tree, if it exists
+        public bool Remove(int element)
+        {
+            if (Contains(element))
+            {
+                root = Remove(root, element);
+                nodeCount--;
+                return true;
+            }
+            return false;
+        }
+
+        private TreeNode Remove(TreeNode node, int element)
+        {
+            if (node == null) return null;
+
+            int cmp = element.CompareTo(node.data);
+
+            // Dig into left subtree, the value we're looking for is smaller than the current value
+            if (cmp < 0)
+                node.left = Remove(node.left, element);
+
+            // Dig into right subtree, the value we're looking for is greater than the current value
+            else if (cmp > 0)
+                node.right = Remove(node.right, element);
+
+            // Found the node we wish to remove
+            else
+            {
+                // This is the case with only a right subtree or no subtree at all. In this situation just
+                // swap the node we wish to remove with its right child
+                if (node.left == null)
+                {
+                    TreeNode rightChild = node.right;
+
+                    node.data = -11111;
+                    node = null;
+
+                    return rightChild;
+                }
+
+                // This is the case with only a left subtree or no subtree at all. In this situation just
+                // swap the node we wish to remove with its left child
+                else if (node.right == null)
+                {
+                    TreeNode leftChild = node.left;
+
+                    node.data = -11111;
+                    node = null;
+
+                    return leftChild;
+                }
+
+                // When removing a node from a binary tree with two links the successor of the node being removed
+                // can either be the largest value in the left subtree or the smallest value in the right subtree.
+                // In this implementation I have decided to find the smallest value in the right subtree which can 
+                // be found by traversing as far left as possible in the right subtree
+                else
+                {
+                    // Find the leftmost node in the right subtree
+                    TreeNode tmp = DigLeft(node.right);
+
+                    // Swap the data
+                    node.data = tmp.data;
+
+                    // Go into the right subtree and remove the leftmost node we found and swapped data with. This 
+                    // prevents us from having two nodes in our tree with the same value.
+                    node.right = Remove(node.right, tmp.data);
+
+                    // If instead we wanted to find the largest node in the left subtree as opposed to smallest node
+                    // in the right subtree here is what we would do:
+                    // Node.tmp = DigRight(node.left);
+                    // Node.data = tmp.data;
+                    // Node.left = Remove(node.left, tmp.data);
+                }
+            }
+
+            return node;
+        }
+
+        // Helper method to find the leftmost node
+        private TreeNode DigLeft(TreeNode node)
+        {
+            TreeNode cur = node;
+
+            while (cur.left != null)
+                cur = cur.left;
+            return cur;
+        }
+
+        // Helper method to find the rightmost node
+        private TreeNode DigRight(TreeNode node)
+        {
+            TreeNode cur = node;
+
+            while (cur.right != null)
+                cur = cur.right;
+            return cur;
+        }
+
+        // Returns true is the element exists in the tree
+        public bool Contains(int element)
+        {
+            return Contains(root, element);
+        }
+
+        // Private recursive method to find an element in the tree
+        private bool Contains(TreeNode node, int element)
+        {
+            // Base case: reached bottom, value not found
+            if (node == null) return false;
+
+            int cmp = element.CompareTo(node.data);
+
+            // Dig into the left subtree because the value we're looking for is smaller than the 
+            // current value
+            if(cmp < 0) return Contains(node.left, element);
+
+            // Dig into the right subtree because the value we're looking for is greater than the 
+            // current value
+            else if(cmp > 0) return Contains(node.right, element);
+
+            // We found the value we were looking for 
+            else return true;
+        }
+
+        // Computes the height of the tree, O(n)
+        public int Height()
+        {
+            return Height(root);
+        }
+
+        // Recursive helper method to compute the height of the tree
+        private int Height(TreeNode node)
+        {
+            if(node == null) return 0;
+            return Math.Max(Height(node.left), Height(node.right)) + 1;
+        }
+    }
+
     #endregion
 }
