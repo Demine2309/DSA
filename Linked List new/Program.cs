c8454b942284464d8229de99adb68ab4fd2ab51c
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-15 02:25:31 +0700

    Demine
    15/04*2024

--- Linked List new/Program.cs
+++ Linked List new/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DSA
+{
+	#region Single Linked List
+	public class Node
+	{
+		public int data;
+		public Node next;
+
+		public Node(int d)
+		{
+			data = d;
+			next = null;
+		}
+	}
+
+	public class SinglyLinkedList
+	{
+		public Node head;
+
+		// The ListLength() function takes a linked list as input and counts the number of node in the list
+		// Time complexity: O(n)
+		public int ListLength(Node head)
+		{
+			Node current = head;
+			int count = 0;
+
+			while(current != null)
+			{
+				count++;
+				current = current.next;
+			}
+
+			return count;
+		}
+
+		// Singly linked list insertion
+		// 1. Inserting a Node in Singly Linked List at the beginning
+
+	}
+	#endregion
+}
