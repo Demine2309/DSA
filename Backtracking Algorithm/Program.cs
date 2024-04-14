af8ce891c3b9b652becbf3db3ef1dd789bc547cd
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-14 16:39:27 +0700

    Demine
    14/04/2024

--- Backtracking Algorithm/Program.cs
+++ Backtracking Algorithm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Google.OrTools.Algorithms;
 
 namespace DSA
 {
@@ -325,8 +326,40 @@ namespace DSA
 
     #region The Knapsack problem
     /*
-     * - In the Knapsack problem, 
+     * - In the Knapsack problem, you need to pack a set of items, with given values and sizes (such as weights or volumes), into a container with a 
+     *   maximum capacity. If the total size of the items exceeds the capacity, you can't pack them all. In that case, the problem is to choose a 
+     *   subset of the items of maximum total value that will fit in the container 
+     * * Step by step
+     * 1. Create the data
+     * - The data includes the following:
+     *      + 'weights': A vector containing the weights of the items
+     *      + 'values': A vector containing the values of the items
+     *      + 'capacities': A vector with just one entry, the capacity of the knapsack
+     * 2. Declare the solver
+     * - The option "KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER' tells the solver to use the branch and bound algorithm to solve the problem.
      */
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            KnapsackSolver solver = new KnapsackSolver(KnapsackSolver.SolverType.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, "KnapsackExample");
+
+            long[] values = {360, 83, 59, 130, 431, 67,  230, 52,  93,  125, 670, 892, 600, 38,  48,  147, 78,
+                  256, 63, 17, 120, 164, 432, 35,  92,  110, 22,  42,  50,  323, 514, 28,  87,  73,
+                  78,  15, 26, 78,  210, 36,  85,  189, 274, 43,  33,  10,  19,  389, 276, 312 };
+           
+            long[,] weights = { { 7,  0,  30, 22, 80, 94, 11, 81, 70, 64, 59, 18, 0,  36, 3,  8,  15,
+                      42, 9,  0,  42, 47, 52, 32, 26, 48, 55, 6,  29, 84, 2,  4,  18, 56,
+                      7,  29, 93, 44, 71, 3,  86, 66, 31, 65, 0,  79, 20, 65, 52, 13 } };
+            
+            long[] capacities = { 850 };
+
+            solver.Init(values, weights, capacities);
+            long computedValue = solver.Solve();
+
+            Console.WriteLine("Optimal Value = " + computedValue);
+        }
+    }
     #endregion
     #endregion
 }
