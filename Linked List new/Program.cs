19ff34989c9197dea57fec00dddb546c3078eec4
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-15 09:50:44 +0700

    Demine
    15/04/2024

--- Linked List new/Program.cs
+++ Linked List new/Program.cs
@@ -1,12 +1,12 @@
 ﻿namespace DSA
 {
 	#region Single Linked List
-	public class Node
+	public class ListNode
 	{
 		public int data;
-		public Node next;
+		public ListNode next;
 
-		public Node(int d)
+		public ListNode(int d)
 		{
 			data = d;
 			next = null;
@@ -15,13 +15,18 @@
 
 	public class SinglyLinkedList
 	{
-		public Node head;
+		private ListNode head;
+
+		public SinglyLinkedList()
+		{
+			head = null;
+		}
 
 		// The ListLength() function takes a linked list as input and counts the number of node in the list
 		// Time complexity: O(n)
-		public int ListLength(Node head)
+		public int ListLength(ListNode head)
 		{
-			Node current = head;
+			ListNode current = head;
 			int count = 0;
 
 			while(current != null)
@@ -34,8 +39,50 @@
 		}
 
 		// Singly linked list insertion
-		// 1. Inserting a Node in Singly Linked List at the beginning
+		// Time complexity: O(n)
+		public void InsertInLinkedList(int data, int position)
+		{
+			ListNode newNode = new ListNode(data);
 
+			if (position < 1)
+			{
+				Console.WriteLine("Invalid Position!");
+				return;
+			}
+
+			if (position == 1 || head == null)
+			{
+				newNode.next = head;
+				head = newNode;
+			}
+			else
+			{
+				ListNode current = head;
+				for(int i = 1; i < position - 1 && current != null; i++)
+				{
+					current = current.next;
+				}
+
+				if(current == null)
+				{
+					Console.WriteLine("Position is out of range!");
+					return;
+				}
+
+				newNode.next = current.next;
+				current.next = newNode;
+			}
+		}
+
+
+	}
+
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+
+		}
 	}
 	#endregion
 }
