dd591b7c01c9e206412d6b11d07c6a1392a13a44
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-16 18:20:45 +0700

    Demine
    16/04/2024

--- Linked List new/Program.cs
+++ Linked List new/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DSA
+﻿using System.Security;
+
+namespace DSA
 {
     #region Single Linked List
     //public class ListNode
@@ -153,97 +155,192 @@
     #endregion
 
     #region Doubly Linked List
-    public class ListNode
-    {
-        public int data;
-        public ListNode next;
-        public ListNode previous;
+    //public class ListNode
+    //{
+    //    public int data;
+    //    public ListNode next;
+    //    public ListNode previous;
 
-        public ListNode(int d)
-        {
-            data = d;
-            next = previous = null;
-        }
-    }
+    //    public ListNode(int d)
+    //    {
+    //        data = d;
+    //        next = previous = null;
+    //    }
+    //}
 
-    public class DoublyLinkedList
-    {
-        private ListNode head;
-        private ListNode tail;
+    //public class DoublyLinkedList
+    //{
+    //    private ListNode head;
+    //    private ListNode tail;
 
-        public DoublyLinkedList()
-        {
-            head = null;
-            tail = null;
-        }
+    //    public DoublyLinkedList()
+    //    {
+    //        head = null;
+    //        tail = null;
+    //    }
 
-        public void Insert(int data, int position)
-        {
-            if(position < 1)
-            {
-                Console.WriteLine("Invalid position!");
-                return;
-            }
+    //    // Doubly Linked List Insertion
+    //    // Time complexity: O(n)
+    //    public void Insert(int data, int position)
+    //    {
+    //        if (position < 1)
+    //        {
+    //            Console.WriteLine("Invalid position!");
+    //            return;
+    //        }
 
-            ListNode newNode = new ListNode(data);
+    //        ListNode newNode = new ListNode(data);
 
-            if(head == null || position == 1)
-            {
-                newNode.next = head;
-                
-                if(head != null)
-                {
-                    head.previous = newNode;
-                }
+    //        if (head == null || position == 1)
+    //        {
+    //            newNode.next = head;
 
-                head = newNode;
+    //            if (head != null)
+    //            {
+    //                head.previous = newNode;
+    //            }
 
-                if(tail == null)
-                {
-                    tail = newNode;
-                }
+    //            head = newNode;
 
-                return;
-            }
+    //            if (tail == null)
+    //            {
+    //                tail = newNode;
+    //            }
 
-            ListNode currentNode = head;
-            int count = 1;
+    //            return;
+    //        }
 
-            while(count < position - 1 && currentNode != null)
-            {
-                currentNode = currentNode.next;
-                count++;
-            }
+    //        ListNode currentNode = head;
+    //        int count = 1;
 
-            if(currentNode == null)
-            {
-                Console.WriteLine("Position out of range!");
-                return;
-            }
+    //        while (count < position - 1 && currentNode != null)
+    //        {
+    //            currentNode = currentNode.next;
+    //            count++;
+    //        }
 
-            newNode.next = currentNode.next;
-            newNode.previous = currentNode;
+    //        if (currentNode == null)
+    //        {
+    //            Console.WriteLine("Position out of range!");
+    //            return;
+    //        }
 
-            if(currentNode.next != null)
-            {
-                currentNode.next.previous = newNode;
-            }
+    //        newNode.next = currentNode.next;
+    //        newNode.previous = currentNode;
 
-            currentNode.next = newNode; 
+    //        if (currentNode.next != null)
+    //        {
+    //            currentNode.next.previous = newNode;
+    //        }
 
-            if(newNode.next == null)
-            {
-                tail = newNode;
-            }
-        }
-    }
+    //        currentNode.next = newNode;
 
-    class MainClass
-    {
-        static void Main(string[] args)
-        {
-            DoublyLinkedList linkedList = new DoublyLinkedList();
-        }
-    }
+    //        if (newNode.next == null)
+    //        {
+    //            tail = newNode;
+    //        }
+    //    }
+
+    //    // Doubly Linked List deletion
+    //    // Time complexity: O(n)
+    //    public void Delete(int position)
+    //    {
+    //        if (position < 1)
+    //        {
+    //            Console.WriteLine("Invalid position!");
+    //            return;
+    //        }
+
+    //        if (head == null)
+    //        {
+    //            Console.WriteLine("List is empty. Nothing to delete!");
+    //            return;
+    //        }
+
+    //        if (position == 1)
+    //        {
+    //            head = head.next;
+
+    //            if (head != null)
+    //            {
+    //                head.previous = null;
+    //            }
+    //            else
+    //            {
+    //                tail = null;
+    //            }
+    //            return;
+    //        }
+
+    //        ListNode current = head;
+    //        int count = 1;
+
+    //        while (count < position && current != null)
+    //        {
+    //            current = current.next;
+    //            count++;
+    //        }
+
+    //        if (current == null)
+    //        {
+    //            Console.WriteLine("Position is out of range!");
+    //            return;
+    //        }
+
+    //        // Adjust the links to skip  the node to be deleted
+    //        if (current.previous != null)
+    //        {
+    //            current.previous.next = current.next;
+    //        }
+    //        if (current.next != null)
+    //        {
+    //            current.next.previous = current.previous;
+    //        }
+
+    //        // Update tail if the last node is deleted
+    //        if (current == tail)
+    //        {
+    //            tail = current.previous;
+    //        }
+    //    }
+
+    //    public void Display()
+    //    {
+    //        ListNode current = head;
+    //        Console.Write("NULL <-");
+    //        while (current != null)
+    //        {
+    //            Console.Write($" {current.data}");
+    //            if (current.next != null)
+    //            {
+    //                Console.Write(" <=>");
+    //            }
+    //            current = current.next;
+    //        }
+    //        Console.WriteLine(" -> NULL");
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        DoublyLinkedList linkedList = new DoublyLinkedList();
+
+    //        linkedList.Insert(23, 1);
+    //        linkedList.Insert(32, 2);
+    //        linkedList.Insert(09, 3);
+    //        linkedList.Insert(90, 4);
+
+    //        linkedList.Display();
+
+    //        linkedList.Delete(2);
+    //        linkedList.Display();
+    //    }
+    //}
+    #endregion
+
+    #region Circular Linked Lists
+
     #endregion
 }
