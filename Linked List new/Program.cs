9b81009a25e1e75baca85ae34c6cf1c42c434242
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-17 03:59:54 +0700

    Demine
    17/04/2024

--- Linked List new/Program.cs
+++ Linked List new/Program.cs
@@ -340,7 +340,70 @@ namespace DSA
     //}
     #endregion
 
-    #region Circular Linked Lists
+    #region Implement stack using Linked List
+    public class ListNode
+    {
+        public int data;
+        public ListNode next;
 
+        public ListNode(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public class Stack
+    {
+        private ListNode top;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public void Push(int data)
+        {
+            var newNode = new ListNode(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        public int Pop()
+        {
+            if(top == null)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            int poppedData = top.data;
+            top = top.next;
+            return poppedData;
+        }
+
+        public int Peek()
+        {
+            if (top == null)
+                throw new InvalidOperationException("Stack is empty.");
+            
+            return top.data;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            myStack.Push(3);
+            myStack.Push(9);
+            myStack.Push(23);
+            myStack.Push(13);
+
+            Console.WriteLine(myStack.Pop());
+
+            Console.WriteLine(myStack.Peek());
+        }
+    }
     #endregion
 }
