63766f9699d19977339a17aa8bd09431ce206268
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-22 23:27:50 +0700

    Demine
    22/04/2024

---
+++ Hashing/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DSA
+{
+	#region First Repeat
+	public class Solution
+	{
+		public char FirstRepeatedChar(string str)
+		{
+			int[] count = new int[256];
+
+			for(int i = 0; i < 256; i++)
+				count[i] = 0;
+			
+			for(int i = 0; i < str.Length; i++)
+			{
+				if (count[str[i]] == 1)
+				{
+					Console.Write(str[i]);
+					break;
+				}
+				else
+					count[str[i]]++;
+			}
+		}
+	}
+
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+
+
+		}
+	}
+	#endregion
+}
