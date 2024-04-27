a3594188896e9ac10b41d6ff4e43795e4d91d646
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-27 15:35:08 +0700

    Demine
    27/04/2024

--- Problems & Solutions (Recursion)/Program.cs
+++ Problems & Solutions (Recursion)/Program.cs
@@ -70,9 +70,72 @@
     #endregion
 
     #region Problem 3: Generate all the strings of n bits. Assume A[0...n-1] is an array of size n
+    //public class Solution
+    //{
+    //    public void GenerateBitStrings(int[] arr, int n)
+    //    {
+    //        if (n < 1)
+    //            PrintArray(arr);
+    //        else
+    //        {
+    //            arr[n - 1] = 0;
+    //            GenerateBitStrings(arr, n - 1);
+    //            arr[n - 1] = 1;
+    //            GenerateBitStrings(arr, n - 1);
+    //        }
+    //    }
+
+    //    private void PrintArray(int[] arr)
+    //    {
+    //        for (int i = arr.Length - 1; i >= 0; i--)
+    //        {
+    //            Console.Write(arr[i]);
+    //        }
+
+    //        Console.WriteLine();
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Solution solution = new Solution();
+
+    //        int n = 3;
+    //        int[] arr = new int[n];
+
+    //        solution.GenerateBitStrings(arr, n);
+    //    }
+    //}
+    #endregion
+
+    #region Problem 4: Generate all the strings of length n drawn from 0...k-1
     public class Solution
     {
-        
+        public void GenerateAllStrings(int[] arr, int n, int k)
+        {
+            if (n < 1)
+                PrintArray(arr);
+            else
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    arr[n - 1] = i;
+                    GenerateAllStrings(arr, n - 1, k);
+                }
+            }
+        }
+
+        private void PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+        }
     }
 
     class MainClass
@@ -81,8 +144,24 @@
         {
             Solution solution = new Solution();
 
+            int n = 3;
+            int k = 9;
 
+            int[] arr = new int[n];
+
+            solution.GenerateAllStrings(arr, n, k);
         }
     }
+    #endregion
+
+    #region Problem 5: Finding the length of connected cells of 1s (regions) in an matrix of 0s and 1s
+    /*
+     * Given a matrix, each of which may be 1 or 0. The filled cells that are connected form a region. 
+     * Two cells are said to be connected if they are adjacent to each other horizontally, vertically 
+     * or diagonally. There may be several regions in the matrix. How do you find the largest region
+     * (in terms of number of cells) in the matrix?
+     */
+
+
     #endregion
 }
